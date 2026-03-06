35e8689f8b4d91f117a8fd9bcc8db95f80a7d98d
Pavel Murlykin <pamurlykin@gmail.com>
2026-03-06 22:53:03 +0300

    Вывод карты на главной странице

--- homepage/tests.py
+++ homepage/tests.py
@@ -1,3 +1,74 @@
-from django.test import TestCase
+from decimal import Decimal
 
-# Create your tests here.
+from django.test import TestCase
+from django.urls import reverse
+
+from property.models import (
+    City,
+    Developer,
+    District,
+    RealEstateClass,
+    RealEstateComplex,
+    RealEstateType,
+    Region,
+)
+
+
+class HomepageIndexViewTests(TestCase):
+    def setUp(self):
+        self.region_spb = Region.objects.create(name='Северо-Запад', code='NW')
+        self.region_msk = Region.objects.create(name='Центральный', code='CTR')
+
+        self.city_spb = City.objects.create(name='Санкт-Петербург', region=self.region_spb)
+        self.city_msk = City.objects.create(name='Москва', region=self.region_msk)
+
+        self.district_spb = District.objects.create(name='Приморский', city=self.city_spb)
+        self.district_msk = District.objects.create(name='Тверской', city=self.city_msk)
+
+        self.developer = Developer.objects.create(name='Developer')
+        self.real_estate_type = RealEstateType.objects.create(name='Квартира')
+        self.real_estate_class = RealEstateClass.objects.create(name='Комфорт', weight=Decimal('1.00'))
+
+        self.complex_spb = RealEstateComplex.objects.create(
+            name='ЖК СПб',
+            map_link='https://yandex.ru/maps/org/spb_complex',
+            developer=self.developer,
+            district=self.district_spb,
+            real_estate_class=self.real_estate_class,
+            real_estate_type=self.real_estate_type,
+        )
+        self.complex_msk = RealEstateComplex.objects.create(
+            name='ЖК Москва',
+            map_link='https://yandex.ru/maps/org/msk_complex',
+            developer=self.developer,
+            district=self.district_msk,
+            real_estate_class=self.real_estate_class,
+            real_estate_type=self.real_estate_type,
+        )
+
+    def test_index_uses_saint_petersburg_by_default(self):
+        response = self.client.get(reverse('homepage:index'))
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['selected_city'], self.city_spb)
+        self.assertEqual(response.context['headline_city'], 'Санкт-Петербурге')
+        self.assertQuerySetEqual(
+            response.context['complexes'],
+            [self.complex_spb],
+            transform=lambda item: item,
+            ordered=False,
+        )
+
+    def test_index_filters_complexes_by_selected_city(self):
+        response = self.client.get(reverse('homepage:index'), {'city': self.city_msk.id})
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['selected_city'], self.city_msk)
+        self.assertEqual(response.context['headline_city'], 'Москве')
+        self.assertQuerySetEqual(
+            response.context['complexes'],
+            [self.complex_msk],
+            transform=lambda item: item,
+            ordered=False,
+        )
+        self.assertContains(response, 'Доступные для расчета ЖК в Москве')
--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,57 @@
 from django.shortcuts import render
 
+from property.models import City, RealEstateComplex
+
+
+CITY_PREPOSITIONAL_CASE = {
+    'Санкт-Петербург': 'Санкт-Петербурге',
+    'Москва': 'Москве',
+    'Калининград': 'Калининграде',
+}
+
 
 def index(request):
+    cities = City.objects.filter(is_active=True).order_by('name')
+    selected_city = None
+
+    city_id = request.GET.get('city')
+    if city_id and city_id.isdigit():
+        selected_city = cities.filter(pk=int(city_id)).first()
+
+    if selected_city is None:
+        selected_city = cities.filter(name='Санкт-Петербург').first() or cities.first()
+
+    complexes = RealEstateComplex.objects.none()
+    if selected_city:
+        complexes = (
+            RealEstateComplex.objects
+            .filter(
+                is_active=True,
+                district__is_active=True,
+                district__city=selected_city,
+                district__city__is_active=True,
+            )
+            .select_related('district__city')
+            .order_by('name')
+        )
+
+    complexes_map_data = [
+        {
+            'name': complex_obj.name,
+            'map_link': complex_obj.map_link or '',
+        }
+        for complex_obj in complexes
+    ]
+
     template = 'homepage/index.html'
-    return render(request, template)
+    context = {
+        'cities': cities,
+        'selected_city': selected_city,
+        'complexes': complexes,
+        'complexes_map_data': complexes_map_data,
+        'headline_city': (
+            CITY_PREPOSITIONAL_CASE.get(selected_city.name, selected_city.name)
+            if selected_city else 'выбранном городе'
+        ),
+    }
+    return render(request, template, context)
